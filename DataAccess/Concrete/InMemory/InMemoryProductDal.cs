5cdbc844af13a3e7d4ee0c56f935d052f7390c2b
Farmin10 <77437205+Farmin10@users.noreply.github.com>
2021-02-14 23:31:01 +0400

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>() {
+            new Product{ProductId=1,CategoryId=1,ProductName="Telefon",UnitPrice=10,UnitsInStock=15 },
+            new Product{ProductId=2,CategoryId=1,ProductName="Komputer",UnitPrice=600,UnitsInStock=35 },
+            new Product{ProductId=3,CategoryId=2,ProductName="Stəkan",UnitPrice=10,UnitsInStock=25 },
+            new Product{ProductId=4,CategoryId=2,ProductName="Qaşıq",UnitPrice=5,UnitsInStock=10 },
+            new Product{ProductId=5,CategoryId=2,ProductName="Boşqab",UnitPrice=14,UnitsInStock=14 },
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete =  _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            _products.Remove(productToDelete);
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetByCategoryId(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+            productToUpdate.CategoryId = product.CategoryId;
+        }
+
+        
+    }
+}
