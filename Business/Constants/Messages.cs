f1c169b410b6ba163ccb19e0b80d7159725a5872
Farmin10 <77437205+Farmin10@users.noreply.github.com>
2021-02-20 21:50:31 +0400

    continue

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product Added";
+        public static string ProductNameInvalid = "Invalid Product Name";
+
+        public static string MaintanaceTime = "Now pur Working Hour";
+
+        public static string ProductListed = "Products Listed";
+    }
+}
